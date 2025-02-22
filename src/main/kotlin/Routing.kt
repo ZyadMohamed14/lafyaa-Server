a9b2c135dffcebfd7696218bb456479754bd629d
ZyadMohamed14 <zyadmuhammed05@gmail.com>
2025-02-22 19:37:08 +0200

    add

---
+++ src/main/kotlin/Routing.kt
@@ -0,0 +1,25 @@
+package com.test
+
+import com.auth0.jwt.JWT
+import com.auth0.jwt.algorithms.Algorithm
+import io.ktor.server.application.*
+import io.ktor.server.auth.*
+import io.ktor.server.auth.jwt.*
+import io.ktor.server.plugins.autohead.*
+import io.ktor.server.response.*
+import io.ktor.server.routing.*
+import io.ktor.server.sse.*
+import io.ktor.sse.*
+
+fun Application.configureRouting() {
+    install(SSE)
+    install(AutoHeadResponse)
+    routing {
+        get("/") {
+            call.respondText("Hello World!")
+        }
+        sse("/hello") {
+            send(ServerSentEvent("world"))
+        }
+    }
+}
---
+++ src/main/kotlin/Security.kt
@@ -0,0 +1,35 @@
+package com.test
+
+import com.auth0.jwt.JWT
+import com.auth0.jwt.algorithms.Algorithm
+import io.ktor.server.application.*
+import io.ktor.server.auth.*
+import io.ktor.server.auth.jwt.*
+import io.ktor.server.plugins.autohead.*
+import io.ktor.server.response.*
+import io.ktor.server.routing.*
+import io.ktor.server.sse.*
+import io.ktor.sse.*
+
+fun Application.configureSecurity() {
+    // Please read the jwt property from the config file if you are using EngineMain
+    val jwtAudience = "jwt-audience"
+    val jwtDomain = "https://jwt-provider-domain/"
+    val jwtRealm = "ktor sample app"
+    val jwtSecret = "secret"
+    authentication {
+        jwt {
+            realm = jwtRealm
+            verifier(
+                JWT
+                    .require(Algorithm.HMAC256(jwtSecret))
+                    .withAudience(jwtAudience)
+                    .withIssuer(jwtDomain)
+                    .build()
+            )
+            validate { credential ->
+                if (credential.payload.audience.contains(jwtAudience)) JWTPrincipal(credential.payload) else null
+            }
+        }
+    }
+}
